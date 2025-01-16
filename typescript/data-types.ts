ecf2433d8faa5b1cd56fdb8ff0b9c80b969d4079
MUSAB MUBARAQ MBURAIMOH <114432105+kingmusab-tmt@users.noreply.github.com>
2025-01-16 15:56:32 +0000

    typescript

---
+++ typescript/data-types.ts
@@ -0,0 +1,23 @@
+let variable = "foo"; // this is a string
+variable = 0; // variable is reassigned as a number
+variable = false; // variable is reassigned as a boolean
+variable = 0.1; // variable is reassigned as a number
+
+let foo = variable + 12;
+foo = foo + " this is twelve and ";
+foo = foo + 0;
+console.log(foo);
+
+
+
+function addATrueNumber(x: number, y: number) {
+    if ((typeof x === "number") && (typeof y === "number")) {
+        return x + y;
+    }
+    throw new Error("One of the inputs is not a number", {
+        cause: "Not a number.",
+    });
+}
+
+const aTrueNumber = addATrueNumber(12, 12);
+console.log(aTrueNumber);
